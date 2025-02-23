b5a02a51ec963d1bfe70b3bc483235428702e616
MariiaIvvanova <ivanova_mariia36@mail.ru>
2025-02-23 20:26:36 +0300

    Изображение Марса

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def promo():
         return html_file.read()
 
 
+@app.route("/image_mars")
+def image_mars():
+    with open("image_mars.html", "r", encoding="utf-8") as html_file:
+        return html_file.read()
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
 
